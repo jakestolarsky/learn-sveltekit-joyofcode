aeb3c5009a3271c2ae6c1b2d4a554a550a56742a
jakestolarsky <stolarczyk.jk@gmail.com>
2026-06-01 14:05:21 +0200

    add simple navigation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,12 @@
-<script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+<nav>
+	<ul>
+		<li>
+			<a href="/">Home</a>
+		</li>
+		<li>
+			<a href="/posts">Posts</a>
+		</li>
+	</ul>
+</nav>
 
-	let { children } = $props();
-</script>
-
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
-
-{@render children()}
+<slot />
