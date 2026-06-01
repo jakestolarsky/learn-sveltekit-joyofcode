073c5b7bc6981e3b7eb6e23d855c99efa0d80ffb
jakestolarsky <stolarczyk.jk@gmail.com>
2026-06-01 16:14:43 +0200

    add prisma schema

---
+++ prisma/seed.ts
@@ -0,0 +1,37 @@
+import { PrismaClient } from '@prisma/client/extension';
+
+const db = new PrismaClient();
+
+type Post = {
+	title: string;
+	body: string;
+};
+
+async function getPosts() {
+	const response = await fetch('http://dummyjson.com/posts');
+	const { posts } = await response.json();
+	return posts as Post[];
+}
+
+function slugify(text: string) {
+	return text
+		.replace(/\s/g, '-')
+		.replace(/[^a-zA-Z0-9]/g, '')
+		.toLowerCase();
+}
+
+async function main() {
+	const posts = await getPosts();
+
+	for (const post of posts) {
+		await db.post.create({
+			data: {
+				title: post.title,
+				content: post.body,
+				slug: slugify(post.title)
+			}
+		});
+	}
+}
+
+main();
