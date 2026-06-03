a7b37a417669ffa66c079371bd8dd2844782d2ef
jakestolarsky <stolarczyk.jk@gmail.com>
2026-06-03 12:20:15 +0200

    create endpoint to get posts from prisma database

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,5 +1,8 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
+import { DATABASE_URL } from '$env/static/private';
+import { PrismaClient } from '../../generated/prisma/client';
 
-const db = new PrismaClient();
+const adapter = new PrismaBetterSqlite3({ url: DATABASE_URL });
+const db = new PrismaClient({ adapter });
 
 export default db;
---
+++ src/routes/api/posts/+server.ts
@@ -0,0 +1,7 @@
+import { json, type RequestHandler } from '@sveltejs/kit';
+import db from '$lib/database';
+
+export const GET: RequestHandler = async () => {
+	const posts = await db.post.findMany();
+	return json(posts);
+};
