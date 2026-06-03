747fffaef2e96852f3582f9dcc1850bda0cde3c7
jakestolarsky <stolarczyk.jk@gmail.com>
2026-06-03 17:46:58 +0200

    add params to link api/post

--- src/routes/api/posts/+server.ts
+++ src/routes/api/posts/+server.ts
@@ -1,7 +1,13 @@
 import { json, type RequestHandler } from '@sveltejs/kit';
 import db from '$lib/database';
 
-export const GET: RequestHandler = async () => {
-	const posts = await db.post.findMany();
+export const GET: RequestHandler = async ({ url }) => {
+	const limit = Number(url.searchParams.get('limit') ?? 20);
+	const order = url.searchParams.get('order') ?? 'asc';
+
+	const posts = await db.post.findMany({
+		orderBy: { id: order },
+		take: limit
+	});
 	return json(posts);
 };
